16c74918d82b2842ebd5e66f961396539e85e470
nopeless <38830903+nopeless@users.noreply.github.com>
2023-11-18 21:32:53 -0600

    fix scene loader

--- src/core/sceneSetup.js
+++ src/core/sceneSetup.js
@@ -6,37 +6,33 @@
 let groundPlane;
 
 function createGroundPlane(texture) {
-    let groundGeometry = new THREE.PlaneGeometry(2000, 4000);
-    let groundMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, map:texture, side: THREE.DoubleSide, transparent: true, opacity: 0.3 });
-    if (!groundMaterial.map) {
-        console.log('Texture not applied to material');
-    }
-    let ground = new THREE.Mesh(groundGeometry, groundMaterial);
-    ground.rotation.x = -Math.PI / 2.2;
-    ground.position.y = -100;
-    ground.receiveShadow = true;
-    return ground;
+  let groundGeometry = new THREE.PlaneGeometry(2000, 4000);
+  let groundMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, map: texture, side: THREE.DoubleSide, transparent: true, opacity: 1 });
+  if (!groundMaterial.map) {
+    console.log('Texture not applied to material');
+  }
+  let ground = new THREE.Mesh(groundGeometry, groundMaterial);
+  ground.rotation.x = -Math.PI / 2.2;
+  ground.position.y = -100;
+  ground.receiveShadow = true;
+  return ground;
 }
-function insideWorld(scene) {
-    let loader = new THREE.TextureLoader();
-    // debugger;
-    const r = loader.load("../assets/textures/groundplane/floor.jpg"
-    , function (texture) {
-        throw 1;
-        if (!texture) {
-            console.log('Texture loading failed');
-            return;
-        }
-        console.log('Texture loaded successfully');
-        groundPlane = createGroundPlane(texture);
-        scene.add(groundPlane);
-        console.log('Ground plane added to scene');
-    }, undefined, function (error) {
-        console.log('An error occurred while loading the texture', error);
-    }
-    );
 
-    console.log(r);
-    // groundPlane = createGroundPlane();
-    // scene.add(groundPlane);
+function insideWorld(scene) {
+  let loader = new THREE.TextureLoader();
+  loader.load("../assets/textures/groundplane/floor.jpg");
+
+  loader.addEventListener("load", function (e) {
+    const texture = e.content;
+    if (!texture) {
+      console.log('Texture loading failed');
+      return;
+    }
+    console.log('Texture loaded successfully', texture);
+    groundPlane = createGroundPlane(texture);
+    scene.add(groundPlane);
+    console.log('Ground plane added to scene');
+  });
+
+  loader.addEventListener("error", console.error.bind(console));
 }
