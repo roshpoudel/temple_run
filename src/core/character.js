d3a617a46a09daf9a975604ba2a7c4c9afb4b7ec
Roshpdl <roxnpoudel445@gmail.com>
2023-11-18 20:53:51 -0600

    temp

---
+++ src/core/character.js
@@ -0,0 +1,77 @@
+// Initialize scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create the character components
+const head = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshBasicMaterial({ color: 0xffd700 }));
+const torso = new THREE.Mesh(new THREE.BoxGeometry(2, 3, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+const arm1 = new THREE.Mesh(new THREE.BoxGeometry(0.5, 3, 0.5), new THREE.MeshBasicMaterial({ color: 0xff6347 }));
+const arm2 = arm1.clone();
+const leg1 = new THREE.Mesh(new THREE.BoxGeometry(0.5, 3, 0.5), new THREE.MeshBasicMaterial({ color: 0x008000 }));
+const leg2 = leg1.clone();
+
+// Position the components
+head.position.y = 4;
+torso.position.y = 2.5;
+arm1.position.y = 2.5;
+arm1.position.x = -1.25;
+arm2.position.y = 2.5;
+arm2.position.x = 1.25;
+leg1.position.y = 0.5;
+leg1.position.x = -0.5;
+leg2.position.y = 0.5;
+leg2.position.x = 0.5;
+
+// Create a group and add the components
+const character = new THREE.Group();
+character.add(head);
+character.add(torso);
+character.add(arm1);
+character.add(arm2);
+character.add(leg1);
+character.add(leg2);
+
+// Add the character to the scene
+scene.add(character);
+
+// Create ground
+const groundGeometry = new THREE.PlaneGeometry(100, 5);
+const groundMaterial = new THREE.MeshBasicMaterial({ color: 0x228B22, side: THREE.DoubleSide });
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = - Math.PI / 2;
+ground.position.y = -1;
+scene.add(ground);
+
+// Set camera position
+camera.position.z = 10;
+
+// Animation variables
+let clock = new THREE.Clock();
+let runningSpeed = 10;
+
+// Animation function
+function animate() {
+    requestAnimationFrame(animate);
+
+    let time = clock.getElapsedTime();
+    let delta = clock.getDelta();
+
+    // Simulate running by rotating arms and legs
+    arm1.rotation.x = Math.sin(time * runningSpeed) * Math.PI / 4;
+    arm2.rotation.x = -Math.sin(time * runningSpeed) * Math.PI / 4;
+    leg1.rotation.x = -Math.sin(time * runningSpeed) * Math.PI / 4;
+    leg2.rotation.x = Math.sin(time * runningSpeed) * Math.PI / 4;
+
+    // Move the ground to simulate running forward
+    ground.position.z = (ground.position.z + delta * 5) % 100;
+
+    renderer.render(scene, camera);
+}
+
+// Start animation loop
+animate();
+
+
---
+++ src/core/main.js
@@ -0,0 +1,158 @@
+/*
+ * main.js
+ * Author: Roshan Poudel, Sherry Khan
+*/
+
+let camera, scene, renderer;
+let cameraControls;
+let clock = new THREE.Clock();
+
+function fillScene() {
+	scene = new THREE.Scene();
+
+	// LIGHTS
+	scene.add(new THREE.AmbientLight(0x333333));
+
+	let light = new THREE.DirectionalLight(0xFFFFFF, 0.9);
+	light.position.set(-1300, 700, 1240);
+	light.castShadow = true;
+
+	scene.add(light);
+
+	light = new THREE.DirectionalLight(0xFFFFFF, 0.7);
+	light.position.set(1000, -500, -1200);
+	light.castShadow = true;
+
+	scene.add(light);
+
+	// MATERIALS
+	let path = "../assets/textures/skybox/";
+	let urls = [path + "px.jpg", path + "nx.jpg",
+				path + "py.jpg", path + "ny.jpg",
+				path + "pz.jpg", path + "nz.jpg"];
+
+	var textureCube = THREE.ImageUtils.loadTextureCube( urls );
+	textureCube.format = THREE.RGBFormat;
+
+	let shader = THREE.ShaderLib.cube;
+	shader.uniforms.tCube.value = textureCube;
+
+	let skyMaterial = new THREE.ShaderMaterial({
+		fragmentShader: shader.fragmentShader,
+		vertexShader: shader.vertexShader,
+		uniforms: shader.uniforms,
+		depthWrite: false,
+		side: THREE.BackSide
+	});
+
+	let sky = new THREE.Mesh(new THREE.CubeGeometry(5000, 5000, 5000), skyMaterial);
+	scene.add(sky);
+
+
+	// groundplane
+	insideWorld(scene);
+
+
+	// CHARACTER
+	// Create and add Steve character to the scene
+	// let gameCharacter = new Character();
+	// let characterMesh = gameCharacter.getMesh();
+	// characterMesh.position.set(0, 0, 0);
+	// characterMesh.rotation.x = -Math.PI / 2.2;
+    // characterMesh.position.y = -100;
+	// characterMesh.receiveShadow = true;
+	// scene.add(characterMesh);
+}
+
+function init() {
+	let canvasWidth = window.innerWidth;
+	let canvasHeight = window.innerHeight;
+
+	// CAMERA
+	camera = new THREE.PerspectiveCamera(75, canvasWidth / canvasHeight, 100, 20000);
+	camera.position.set(0, 2000, 4000);
+
+	// RENDERER
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(canvasWidth, canvasHeight);
+	renderer.setClearColorHex(0xffffff, 1.0);
+	renderer.gammaInput = true;
+	renderer.gammaOutput = true;
+	renderer.shadowMapEnabled = true;
+
+	// CONTROLS
+	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
+	cameraControls.target.set(0, -160, 0);
+
+	// Screen Resize
+	window.addEventListener('resize', onWindowResize, false);
+}
+
+// EVENT HANDLERS
+function addToDOM() {
+	let container = document.getElementById('container');
+	let canvas = container.getElementsByTagName('canvas');
+	if (canvas.length > 0) {
+		container.removeChild(canvas[0]);
+	}
+	container.appendChild(renderer.domElement);
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	render();
+}
+
+function render() {
+	let delta = clock.getDelta();
+	cameraControls.update(delta);
+
+	renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	render();
+}
+
+
+// function createGround(texture) {
+// 	let groundGeometry = new THREE.PlaneGeometry(2000, 4000);
+// 	let groundMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, map: texture, side: THREE.DoubleSide, transparent: false, opacity: 1 });
+// 	if (!groundMaterial.map) {
+// 		console.log('Texture not applied to material');
+// 	}
+// 	let ground = new THREE.Mesh(groundGeometry, groundMaterial);
+// 	ground.rotation.x = -Math.PI / 2.2;
+// 	ground.position.y = -100;
+// 	scene.add(ground);
+// 	render();
+// }
+
+// //loading textures
+// let loader = new THREE.TextureLoader();
+// loader.load("../assets/textures/groundplane/floor.jpg", function (texture) {
+// 	if (!texture) {
+// 		console.log('Texture loading failed');
+// 		return;
+// 	}
+// 	console.log('Texture loaded successfully');
+// 	createGround(texture);
+// });
+
+console.log(1)
+
+try {
+	console.log(2)
+	init();
+	fillScene();
+	addToDOM();
+	animate();
+} catch (e) {
+	console.log(e);
+	let errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
+	errorReport += "<pre>" + e.stack + "</pre><br>";
+	document.getElementById('container').innerHTML = errorReport;
+}
